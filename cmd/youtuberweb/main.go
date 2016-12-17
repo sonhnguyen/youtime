a0c0f5330f1e428f25b137d6b9128a957953e063
sonhnguyen <nhson@floatingcube.com>
2016-12-17 14:43:17 +0700

    env vars setup

--- cmd/youtuberweb/main.go
+++ cmd/youtuberweb/main.go
@@ -9,13 +9,10 @@ import (
 	"runtime"
 
 	"github.com/gorilla/context"
-	"github.com/gorilla/sessions"
 	"github.com/justinas/alice"
 	"github.com/kardianos/osext"
 	"github.com/spf13/viper"
 )
-
-const isDevelopment = true
 
 type youtuberConfig struct {
 	Port   string
@@ -25,14 +22,9 @@ type youtuberConfig struct {
 // App in main app
 type App struct {
 	router   *Router
-	store    *sessions.CookieStore
 	gp       globalPresenter
 	logr     appLogger
 	youtuber youtuberConfig
-}
-
-func (a *App) GetStore() *sessions.CookieStore {
-	return a.store
 }
 
 // globalPresenter contains the fields neccessary for presenting in all templates
@@ -53,10 +45,10 @@ func SetupApp(r *Router, logger appLogger, templateDirectoryPath string) *App {
 		Port:   viper.GetString("port"),
 		Config: viper.GetStringMapString("youtuber"),
 	}
+	fmt.Println(viper.GetString("port"))
 	return &App{
 		router:   r,
 		gp:       gp,
-		store:    sessions.NewCookieStore([]byte(youtuberConfig.cookieSecret)),
 		logr:     logger,
 		youtuber: youtuberConfig,
 	}
@@ -68,23 +60,23 @@ func main() {
 		log.Fatalf("cannot retrieve present working directory: %i", 0600, nil)
 	}
 	err = LoadConfiguration(pwd)
-	if err != nil && viper.GetBool("isProduction") {
+	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	//TODO config static file path and template file path
 
 	r := NewRouter()
 	logr := newLogger()
 	a := SetupApp(r, logr, "")
-	port, found := os.Getenv("PORT")
-	if !found {
+	port := os.Getenv("PORT")
+	if port == "" {
 		port = a.youtuber.Port
+		fmt.Println("port from file", port)
 	}
 
 	common := alice.New(context.ClearHandler, a.loggingHandler, a.recoverHandler)
 	r.Get("/video/youtube/:id", common.Then(a.Wrap(a.GetYoutubeHandler())))
 	r.Post("/video/youtube/:id", common.Then(a.Wrap(a.PostYoutubeHandler())))
-
+	fmt.Println("port from var config", port)
 	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		fmt.Errorf("error on serve server %s", err)
@@ -94,11 +86,11 @@ func main() {
 func LoadConfiguration(pwd string) error {
 	viper.SetConfigName("youtime-config")
 	viper.AddConfigPath(pwd)
-	devPath := pwd[:len(pwd)-3] + "/src/youtuber/cmd/youtuberweb/"
+	devPath := pwd[:len(pwd)-3] + "src/youtuber/cmd/youtuberweb/"
 	_, file, _, _ := runtime.Caller(1)
 	configPath := path.Dir(file)
 	viper.AddConfigPath(devPath)
 	viper.AddConfigPath(configPath)
-	viper.SetDefault("path", devPath)
+	fmt.Println("all keys", pwd, devPath, configPath)
 	return viper.ReadInConfig() // Find and read the config file
 }
